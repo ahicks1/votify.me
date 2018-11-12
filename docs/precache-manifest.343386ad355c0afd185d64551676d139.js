e6b7fb3d9de701c741a03e68c814d36141fd491f
ahicks1 <ahicks1@trinity.edu>
2018-11-12 17:00:19 -0600

    Added docs for github pages deploy

---
+++ docs/precache-manifest.343386ad355c0afd185d64551676d139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2031ad6f23bf70f57fe",
+    "url": "/static/js/main.c2031ad6.chunk.js"
+  },
+  {
+    "revision": "d62af70bebb8ed13f580",
+    "url": "/static/js/1.d62af70b.chunk.js"
+  },
+  {
+    "revision": "c2031ad6f23bf70f57fe",
+    "url": "/static/css/main.992d84a9.chunk.css"
+  },
+  {
+    "revision": "d62af70bebb8ed13f580",
+    "url": "/static/css/1.469f3e71.chunk.css"
+  },
+  {
+    "revision": "aa34bbc155bef6355cb21aaf64dbcb60",
+    "url": "/index.html"
+  }
+];
