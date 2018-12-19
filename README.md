a580388321af6c51ffafcd33b6e631d5f7a158de
ahicks1 <ahicks1@trinity.edu>
2018-12-19 00:42:06 -0700

    improved landing page and added favicons

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # votify.me
 SPA voting tool with an AWS backend
+
+# Deploying Lambdas:
+## Prerequisites:
+* [aws-cli](https://aws.amazon.com/cli/)
+
+To deploy a lambda run the script `./deployLambda <function name>`
+# Front End
+This was created using [create-react-app](https://facebook.github.io/create-react-app/).
+
+The project is located in the `./votify.me` subdirectory
+## Prerequisites:
+* [aws-cli](https://aws.amazon.com/cli/)
+* Node > 8.0
+* NPM > 5.2
+
+Setup all dependancies with `npm install`
+
+To start the development server run `npm start`
+
+To build for production run `npm run-script build`
+
+To sync with aws run: `aws s3 sync . s3://votify.me.development --acl public-read`
+
--- votify.me/src/App.js
+++ votify.me/src/App.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, withRouter, Redirect } from 'react-router-dom';
+import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link, 
+  withRouter, 
+  Redirect } from 'react-router-dom';
 import Amplify, { Auth } from 'aws-amplify';
 import { Authenticator } from 'aws-amplify-react';
-import { ConfirmSignIn, ConfirmSignUp, ForgotPassword, RequireNewPassword, SignIn, VerifyContact} from 'aws-amplify-react';
+import { 
+  ConfirmSignIn, 
+  ConfirmSignUp, 
+  ForgotPassword, 
+  RequireNewPassword, 
+  SignIn, 
+  VerifyContact } from 'aws-amplify-react';
 
+//material-ui imports
 import './App.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -17,7 +29,7 @@ import Typography from '@material-ui/core/Typography';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
-
+//Local imports
 import ElectionsPage from './ElectionsPage'
 import ResultsPage from './ResultsPage'
 import AddElectionPage from './AddElectionPage'
@@ -63,7 +75,9 @@ const theme = createMuiTheme({
   },
 });
 
-
+/**
+ * Hods the whole app including the router
+ */
 class App extends React.Component {
 
   render() {
@@ -83,6 +97,10 @@ class App extends React.Component {
   };
 } 
 
+/**
+ * Used to layout grid for pages
+ * @param {React.Component} WrappedComponent 
+ */
 function withGrid(WrappedComponent) {
     return <div style={{flexGrow:1}}>
     <Grid container justify="center">
@@ -104,23 +122,31 @@ class Home extends React.Component {
   }
   
   render(){
+    let {code} = this.state;
+
     return withGrid(<>
-    <Typography variant="h2" gutterBottom>Welcome to Votify.me</Typography>
-    <TextField label="Election Name:" onChange={e => this.setState({code:e.target.value})} value={this.state.code}/>
-    <br/>
-    <Button component={Link} to={`/vote/${this.state.code}`}>
-      Link
+     <Grid container justify="center">
+      <Grid item ><img item alt="logo" width='200' src={require('./LogoSVG.svg')}></img></Grid></Grid>
+      <Typography align='center' variant="h2" gutterBottom>Welcome to Votify.me</Typography>
+      <Typography align='center' variant="h5" gutterBottom>Make simple <a rel="noopener noreferrer" target="_blank" href="https://en.wikipedia.org/wiki/Borda_count">Borda count</a> polls</Typography>
+      <Grid container justify="center">
+      <Grid item >
+      <Button component={Link} to={`/add-election`}>
+        Get started
     </Button>
+    </Grid>
+    </Grid>
     </>);
   } 
 }
 
-const ThanksPageGrid = (props) => {
-  return withGrid(<ThanksPage {...props} />)
-}
-const SignInPage = (props) => {
-  return <div>{props.authData && <div> <Redirect to='/elections'/> </div>}</div>
-}
+const ThanksPageGrid = (props) => withGrid(<ThanksPage {...props} />)
+
+const SignInPage = ({authData}) => <>
+  {authData && <> <Redirect to='/elections'/> </>}
+</>
+
+
 const SignInAuth = (props) => {
   return <Authenticator hideDefault={true} theme={ampTheme}>
     <SignIn/>
@@ -175,46 +201,37 @@ const ResultsAuth = (props) => {
 
 
 const VoteNav = (props) => {
-  return <div>
-    
-    <Route path={`${props.match.path}/:id`} render= {(p) => 
-    
-        withGrid(<VotePage {...p}/>)} />
+  return <>
+    <Route 
+      path={`${props.match.path}/:id`} 
+      render={(p) => withGrid(<VotePage {...p} />)} 
+    />
     <Route
       exact
       path={props.match.path}
-      render={() => <ElectionsAuth {...props}/>}
+      render={() => <ElectionsAuth {...props} />}
     />
-    </div>;
+  </>;
 }
 
 
 const ElectionsNav = (props) => {
-  return <div>
-    
-    <Route path={`${props.match.path}/:id`} render= {(p) => <ResultsAuth {...p}/>} />
+  return <>
+    <Route 
+      path={`${props.match.path}/:id`} 
+      render= {(p) => <ResultsAuth {...p}/>} 
+    />
     <Route
       exact
       path={props.match.path}
       render={() => <ElectionsAuth {...props}/>}
     />
-    </div>;
+    </>;
 }
 
-
-class Topic extends React.Component{ 
-  constructor(props) {
-    super(props);
-    this.state = {msg:'hello',props:props}
-    console.log(JSON.stringify(props))
-  }
-  render() {return <h3>Requested Param: {this.props.match.params.id}</h3>;}
-};
-const linkStyle = {
-  color: 'inherit',
-  textDecoration: 'inherit'
-};
-
+/**
+ * Component that shows navigation and login information on all pages
+ */
 class Header extends React.Component {
   state = {
     auth: true,
@@ -223,6 +240,7 @@ class Header extends React.Component {
 
   handleMenu = event => {
     this.setState({ anchorEl: event.currentTarget });
+    //TODO: Use Hub to handle auth events
     Auth.currentAuthenticatedUser()
     .then(user => this.setState({auth:true}))
     .catch(err => this.setState({auth:false}));
@@ -230,81 +248,101 @@ class Header extends React.Component {
 
   handleClose = (loc) => {
     this.setState({ anchorEl: null });
-    if(loc) this.props.history.push(loc);
   };
 
   handleSignOut = () => {
     Auth.signOut()
-    this.props.history.push('/')
     this.setState({ anchorEl: null });
+    this.props.history.push('/')
+    
   }
 
     
     render() {
-      const { anchorEl } = this.state;
+      const { anchorEl,auth } = this.state;
       const open = Boolean(anchorEl);
-      let auth = false
-     
-      return <AppBar elevation="0" color="inherit" position="static" style={{flexGrow:1,width:'100%'}}>
+
+      return <AppBar 
+        elevation="0" 
+        color="inherit" 
+        position="static" 
+        style={{flexGrow:1,width:'100%'}}>
           <Toolbar>
             
-            <Link color='inerit' to="/" style={{textDecoration: 'none',flexGrow:1}}><Typography variant="h6" color='secondary' >Votify.me</Typography></Link>
+            <Link 
+              color='inerit' 
+              to="/" style={{textDecoration: 'none',flexGrow:1}}
+            >
+              <Typography variant="h6" color='secondary' >Votify.me</Typography>
+            </Link>
+        <div>
+          <IconButton
+            aria-owns={open ? 'menu-appbar' : undefined}
+            aria-haspopup="true"
+            color='secondary'
+            onClick={this.handleMenu}
+          >
+            <AccountCircle />
+          </IconButton>
+          {auth && (<Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={open}
+            onClose={this.handleClose}>
 
-            
-                <div>
-                  <IconButton
-                    aria-owns={open ? 'menu-appbar' : undefined}
-                    aria-haspopup="true"
-                    color='secondary'
-                    onClick={this.handleMenu}
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                  {this.state.auth && (<Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={this.handleClose}
-                  >
-                  
-                    
-                    <MenuItem onClick={this.handleClose} component={Link} to='/elections'>Your Elections</MenuItem>
-                    <MenuItem onClick={this.handleClose} component={Link} to='/add-election'>Create New Election</MenuItem>
-                    <MenuItem onClick={this.handleSignOut}>Sign Out</MenuItem>
-                    
-                    )
+
+            <MenuItem 
+              onClick={this.handleClose} 
+              component={Link} to='/elections'>
+            Your Elections</MenuItem>
+
+            <MenuItem 
+              onClick={this.handleClose} 
+              component={Link} to='/add-election'
+            >
+              Create New Election</MenuItem>
+            <MenuItem 
+              onClick={this.handleSignOut}
+            >
+              Sign Out
+            </MenuItem>)
+          </Menu>)}
+
+            {!auth && (<Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={this.handleClose}
+            >
+              <MenuItem 
+                onClick = {this.handleClose}
+                component = {Link} 
+                to='/sign-in'
+              >Sign In</MenuItem>
+
+            )
                   </Menu>)}
-                  {!this.state.auth && (<Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={this.handleClose}
-                  >
-                    <MenuItem onClick={() => this.handleClose('/sign-in')}>Sign In</MenuItem>
-                    
-                    )
-                  </Menu>)}
-                </div>
-              
-          </Toolbar>
-        </AppBar>
-      }
+        </div>
+
+      </Toolbar>
+    </AppBar>
+  }
 
     }
     const AuthHeader = withRouter(Header);
